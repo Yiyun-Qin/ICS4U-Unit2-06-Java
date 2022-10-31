9e05de292aa0b57bad72f18c6cd05b19e5682966
Yiyun Qin <yiyun.qin@mths.ca>
2022-10-31 21:17:31 +0000

    airplane

--- FlyTest.java
+++ FlyTest.java
@@ -1,35 +1,32 @@
 /**
  * The 2-06 program with lesson 3.
  *
- * @author  Mr. Coxall
+ * @author  Yiyun Qin
  * @version 1.0
- * @since   2020-01-01
+ * @since   2022-10-31
  */ 
 public class FlyTest {
+  /**
+   * a comment.
+   */
   public static void main(String[] args) {
     Airplane biplane = new Airplane();
     biplane.setSpeed(212);
     System.out.println(biplane.getSpeed());
     Jet boeing = new Jet();
     boeing.setSpeed(422);
-    /**
-     * here is 844.
-     */
     System.out.println(boeing.getSpeed());
     int x = 0;
     while (x < 4) {
       boeing.accelerate();
-      /**
-       * here is 844 * 2 = 1688.
-       */
       System.out.println(boeing.getSpeed());
       if (boeing.getSpeed() > 5000) {
-        boeing.setSpeed(boeing.getSpeed() * 2);
+        biplane.setSpeed(biplane.getSpeed() * 2);
       } else {
         boeing.accelerate();
       }
       x++;
     }
-    System.out.println(boeing.getSpeed());
+    System.out.println(biplane.getSpeed());
   }
 }
